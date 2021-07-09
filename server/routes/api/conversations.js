e85301852e061aab83cfd155c06543a036684048
briones-gabriel <dossantosgabrielb@gmail.com>
2021-07-08 22:21:05 -0300

    Code improvement for the api/conversations file

--- server/routes/api/conversations.js
+++ server/routes/api/conversations.js
@@ -3,7 +3,7 @@ const { User, Conversation, Message } = require("../../db/models");
 const { Op } = require("sequelize");
 const onlineUsers = require("../../onlineUsers");
 
-// get all conversations for a user, include latest message text for preview, and all messages
+// get all conversations for a user, include latest message text for preview, and all user's messages
 // include other user model so we have info on username/profile pic (don't include current user info)
 // TODO: for scalability, implement lazy loading
 router.get("/", async (req, res, next) => {
@@ -53,8 +53,7 @@ router.get("/", async (req, res, next) => {
       ],
     });
 
-    for (let i = 0; i < conversations.length; i++) {
-      const convo = conversations[i];
+    conversations.forEach((convo, i) => {
       const convoJSON = convo.toJSON();
 
       // set a property "otherUser" so that frontend will have easier access
@@ -67,19 +66,16 @@ router.get("/", async (req, res, next) => {
       }
 
       // set property for online status of the other user
-      if (onlineUsers.includes(convoJSON.otherUser.id)) {
-        convoJSON.otherUser.online = true;
-      } else {
-        convoJSON.otherUser.online = false;
-      }
+      convoJSON.otherUser.online = onlineUsers.includes(convoJSON.otherUser.id);
 
       // set properties for notification count and latest message preview
       convoJSON.latestMessageText = convoJSON.messages[0].text;
-      conversations[i] = convoJSON;
 
       // set the correct order for messages to be displayed
       convoJSON.messages.reverse();
-    }
+
+      conversations[i] = convoJSON;
+    });
 
     res.json(conversations);
   } catch (error) {
